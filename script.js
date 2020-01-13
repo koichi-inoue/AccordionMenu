a6fd36c3a58025496b113a336c8e4eaa9947aad5
koichi-inoue <inoue.ko@gmail.com>
2020-01-13 15:06:01 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
 $(function(){
 
-  $("#MenuButton").click( function(){
-    $("#MenuList").slideToggle();
+  // Intialize
+  $(".menu").hide();
+  $(".menu-child").hide();
+  $('.menu>li').has('.menu-child').append('<a href="#" class="dropButton"></a>');
+  $(".dropButton").addClass('isClosed');
+
+  // Toggle navButton
+  $(".navButton").click( function(){
+    $(".menu").slideToggle();
   });
 
-  $("ul.subs").hide();
+  // Toggle dropButton
+  $(".dropButton").click( function() {
 
-  $("ul.main").click(function(){
-    $("ul.subs").slideUp();
-      if($("+ul",this).css("display")=="none"){
-        $("+ul",this).slideDown();
-      }
+    if( $(this).hasClass('isClosed')) {
+
+      $(this).removeClass('isClosed');
+      $(this).addClass('isOpen');
+      $(this).prev(".menu-child").slideDown(500);
+
+      $(".dropButton").not(this).removeClass('isOpen');
+      $(".dropButton").not(this).addClass('isClosed');
+      $(".dropButton").not(this).prev(".menu-child").slideUp(500);
+
+    } else {
+
+      $(this).removeClass('isOpen');
+      $(this).addClass('isClosed');
+      $(this).prev(".menu-child").slideUp(500);
+
+    }
   });
 
 });
